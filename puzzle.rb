cb2364c7edb7bc8b5d64f6fa07fb254cb4d7068e
Douglas Roper <douglasroper@notonthehighstreet.com>
2012-11-13 17:44:48 +0000

    solved

--- puzzle.rb
+++ puzzle.rb
@@ -36,92 +36,64 @@ class Grid
     @fitted_pieces = []
   end
 
-  def solve
-    @pieces.each do |piece|
-    if piece_fits?(piece)
-      @fitted_pieces << piece
+  def solve(loc)
+    return true if @pieces.empty?
+    @pieces.each_with_index do |e, i|
+      if @fitted_pieces[loc] == nil
+        @fitted_pieces[loc] = @pieces.shift
+      else
+        next
+      end
+
+      (0..4).each do |k|
+        if piece_fits?(@fitted_pieces[loc], loc) && solve(loc + 1)
+          return true;
+        else
+          @fitted_pieces[loc].rotate
+        end
+      end
+      @pieces << @fitted_pieces.pop
+    end
+    false
+  end
+
+  def piece_fits?(piece, location)
+    case location
+    when 0
+      true
+    when 1, 2
+      left_right_match?(piece, location)
+    when 3
+      piece.top + @fitted_pieces[0].bottom == 0
+    when 4
+      piece.top + @fitted_pieces[1].bottom == 0 && left_right_match?(piece, location)
+    when 5
+      piece.top + @fitted_pieces[2].bottom == 0 && left_right_match?(piece, location)
+    when 6
+      piece.top + @fitted_pieces[3].bottom == 0
+    when 7
+      piece.top + @fitted_pieces[4].bottom == 0 && left_right_match?(piece, location)
+    when 8
+      piece.top + @fitted_pieces[5].bottom == 0 && left_right_match?(piece, location)
     end
   end
 
-  # def solve
-  #   @location = 0
-  #   @tries = @pieces.length
-  #   while @pieces.length != 0
-  #     piece = @pieces.shift
-
-  #     (0..4).each do |i|
-  #       if piece_fits?(piece, @location)
-  #         @fitted_pieces << piece
-  #         @location += 1
-  #         break
-  #       else
-  #         piece.rotate
-  #         if i == 4
-  #           (0..4).each do |j|
-  #             if piece_fits?(piece, @location)
-  #             @fitted_pieces.last.rotate
-  #             if j == 4
-  #               @pieces << @fitted_pieces
-  #               @pieces << piece
-  #               @location -= 1
-  #               break
-  #             end
-  #           end
-  #         end
-  #       end
-  #     end
-  #   end
-  #   @fitted_pieces.to_s
-  # end
-
-  # def piece_fits?(piece, location)
-  #   case location
-  #   when 0
-  #     true
-  #   when 1, 2
-  #     left_right_match?(piece, location)
-  #   when 3
-  #     piece.top + @fitted_pieces[0].bottom == 0
-  #   when 4
-  #     piece.top + @fitted_pieces[1].bottom == 0 && left_right_match?(piece, location)
-  #   when 5
-  #     piece.top + @fitted_pieces[2].bottom == 0 && left_right_match?(piece, location)
-  #   when 6
-  #     piece.top + @fitted_pieces[3].bottom == 0
-  #   when 7
-  #     piece.top + @fitted_pieces[4].bottom == 0 && left_right_match?(piece, location)
-  #   when 8
-  #     piece.top + @fitted_pieces[5].bottom == 0 && left_right_match?(piece, location)
-  #   end
-  # end
-
-  # def left_right_match?(piece, location)
-  #   piece.left + @fitted_pieces[location - 1].right == 0
-  # end
-
-  # def top_bottom_macth(piece)
-  # end
+  def left_right_match?(piece, location)
+    piece.left + @fitted_pieces[location - 1].right == 0
+  end
 end
 
 
-
+# piece_1 = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
+# piece_2 = Piece.new([SPADE, INNER_SPADE, INNER_CLUBS, HEART], 1)
+# piece_3 = Piece.new([DIAMOND, INNER_SPADE, INNER_HEART, SPADE], 2)
+# piece_4 = Piece.new([DIAMOND, INNER_DIAMOND, INNER_HEART, HEART], 3)
+# piece_5 = Piece.new([CLUBS, INNER_CLUBS, INNER_DIAMOND, DIAMOND], 4)
+# piece_6 = Piece.new([HEART, INNER_SPADE, INNER_HEART, CLUBS], 5)
+# piece_7 = Piece.new([SPADE, INNER_HEART, INNER_CLUBS, SPADE], 6)
+# piece_8 = Piece.new([DIAMOND, INNER_CLUBS, INNER_CLUBS, HEART], 7)
+# piece_9 = Piece.new([HEART, INNER_DIAMOND, INNER_CLUBS, CLUBS], 8)
 
 # grid = Grid.new([piece_1, piece_2,piece_3,piece_4,piece_5,piece_6,piece_7,piece_8,piece_9])
 
-# grid.solve
-
-
-
-#while pieces is not empty
-
-    #piece = pop pieces
-
-#    while piece does not fit
-#        rotate;
-#        num_rotations++
-#        if num rotations == 4
-#            push piece onto pieces
-#            pop next piece from pieces
-#    else
-#        put piece into grid
-#circular shift pieces
+# grid.solve(0)
--- spec/puzzle_spec.rb
+++ spec/puzzle_spec.rb
@@ -1,112 +1,131 @@
 
 require_relative "spec_helper"
 
-describe Piece do
-  subject {Piece.new([1,2,3,4], 1)}
-  it "has top, right, left and right" do
-    subject.top.should == 1
-    subject.right.should == 2
-    subject.bottom.should == 3
-    subject.left.should == 4
+# describe Piece do
+#   subject {Piece.new([1,2,3,4], 1)}
+#   it "has top, right, left and right" do
+#     subject.top.should == 1
+#     subject.right.should == 2
+#     subject.bottom.should == 3
+#     subject.left.should == 4
+#   end
+
+#   it "rotates" do
+#     subject.rotate
+#     subject.top.should == 2
+#     subject.right.should == 3
+#     subject.bottom.should == 4
+#     subject.left.should == 1
+#   end
+
+#   it "to string" do
+#     subject.to_s.should == "[\"heart\", \"spade\", \"diamond\", \"clubs\"]"
+#     subject.rotate
+#     subject.to_s.should == "[\"spade\", \"diamond\", \"clubs\", \"heart\"]"
+#   end
+# end
+
+
+# describe Grid do
+#   it "has no fitted pieces" do
+#     Grid.new([stub]).fitted_pieces.should == []
+#   end
+
+#   describe "#piece_fits?" do
+#     it "checks if the piece fits in the first location" do
+#       piece = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
+#       Grid.new(piece).piece_fits?(piece, 0).should be_true
+#     end
+
+#     it "doesn't fit in the second location on the top" do
+#       piece = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
+#       grid = Grid.new(piece)
+#       grid.fitted_pieces = [piece]
+#       grid.piece_fits?(piece, 1).should be_false
+#     end
+
+#     it "does fit in the second location on the top" do
+#       piece = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
+#       piece2 = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, HEART], 0)
+#       grid = Grid.new(piece)
+#       grid.fitted_pieces = [piece, piece]
+#       grid.piece_fits?(piece2, 1).should be_true
+#       grid.piece_fits?(piece2, 2).should be_true
+#     end
+
+#     it "does fit the 5th location on the top and left" do
+#       piece = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
+#       piece2 = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, HEART], 0)
+#       grid = Grid.new(piece)
+#       grid.fitted_pieces = [piece, piece, piece, piece, piece]
+#       grid.piece_fits?(piece2, 5).should be_true
+#     end
+
+#     it "does fit in the 4 location on the top" do
+#       piece = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
+#       piece2 = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, HEART], 0)
+#       grid = Grid.new(piece)
+#       grid.fitted_pieces = [piece, piece, piece, piece]
+#       grid.piece_fits?(piece2, 4).should be_true
+#     end
+#   end
+
+#   describe "#solve" do
+#     # subject {set_up_grid}
+#     it "moves to the next location" do
+#       piece_1 = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
+#       piece_2 = Piece.new([SPADE, INNER_SPADE, INNER_CLUBS, HEART], 1)
+#       grid = Grid.new([piece_1, piece_2])
+#       grid.solve
+#       grid.fitted_pieces.to_s.should == "[[\"diamond\", \"i_heart\", \"i_diamond\", \"spade\"], [\"spade\", \"i_spade\", \"i_clubs\", \"heart\"]]"
+#       grid.location.should == 2
+#     end
+
+#     it "rotates the piece to make it fit to the next location" do
+#       piece_1 = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
+#       piece_2 = Piece.new([INNER_SPADE, INNER_CLUBS, HEART, SPADE], 1)
+#       grid = Grid.new([piece_1, piece_2])
+#       grid.solve
+#       grid.fitted_pieces.to_s.should == "[[\"diamond\", \"i_heart\", \"i_diamond\", \"spade\"], [\"spade\", \"i_spade\", \"i_clubs\", \"heart\"]]"
+#       grid.location.should == 2
+#     end
+
+#     it "moves the piece to the last position if it doesn't fit" do
+#       piece_1 = Piece.new([INNER_SPADE, INNER_HEART, INNER_DIAMOND, SPADE], 0)
+#       piece_2 = Piece.new([INNER_SPADE, INNER_CLUBS, SPADE, SPADE], 1)
+#       piece_3 = Piece.new([INNER_SPADE, INNER_CLUBS, HEART, CLUBS], 2)
+#       grid = Grid.new([piece_1, piece_2, piece_3])
+#       grid.solve
+#       grid.fitted_pieces.map(&:id).should == [0,2,1]
+#     end
+
+#     it "moves the first piece to the last position if it doesn't fit" do
+#       piece_1 = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
+#       piece_2 = Piece.new([INNER_SPADE, INNER_CLUBS, CLUBS, SPADE], 1)
+#       piece_3 = Piece.new([INNER_SPADE, INNER_CLUBS, HEART, CLUBS], 2)
+#       grid = Grid.new([piece_2, piece_3, piece_1])
+#       grid.solve
+#       grid.fitted_pieces.map(&:id).should == [0,2,1]
+#     end
+#   end
+# end
+
+describe "Solve" do
+
+  it "solves 3 places" do
+    # piece_1 = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
+    # piece_2 = Piece.new([SPADE, INNER_SPADE, INNER_CLUBS, HEART], 1)
+    # piece_3 = Piece.new([DIAMOND, INNER_SPADE, INNER_HEART, SPADE], 2)
+    # a = Grid.new([piece_1, piece_2, piece_3])
+    # a.solve(0)
+    # a.fitted_pieces.should == [piece_1, piece_2, piece_3]
   end
 
-  it "rotates" do
-    subject.rotate
-    subject.top.should == 2
-    subject.right.should == 3
-    subject.bottom.should == 4
-    subject.left.should == 1
-  end
-
-  it "to string" do
-    subject.to_s.should == "[\"heart\", \"spade\", \"diamond\", \"clubs\"]"
-    subject.rotate
-    subject.to_s.should == "[\"spade\", \"diamond\", \"clubs\", \"heart\"]"
-  end
-end
-
-
-describe Grid do
-  it "has no fitted pieces" do
-    Grid.new([stub]).fitted_pieces.should == []
-  end
-
-  describe "#piece_fits?" do
-    it "checks if the piece fits in the first location" do
-      piece = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
-      Grid.new(piece).piece_fits?(piece, 0).should be_true
-    end
-
-    it "doesn't fit in the second location on the top" do
-      piece = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
-      grid = Grid.new(piece)
-      grid.fitted_pieces = [piece]
-      grid.piece_fits?(piece, 1).should be_false
-    end
-
-    it "does fit in the second location on the top" do
-      piece = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
-      piece2 = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, HEART], 0)
-      grid = Grid.new(piece)
-      grid.fitted_pieces = [piece, piece]
-      grid.piece_fits?(piece2, 1).should be_true
-      grid.piece_fits?(piece2, 2).should be_true
-    end
-
-    it "does fit the 5th location on the top and left" do
-      piece = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
-      piece2 = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, HEART], 0)
-      grid = Grid.new(piece)
-      grid.fitted_pieces = [piece, piece, piece, piece, piece]
-      grid.piece_fits?(piece2, 5).should be_true
-    end
-
-    it "does fit in the 4 location on the top" do
-      piece = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
-      piece2 = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, HEART], 0)
-      grid = Grid.new(piece)
-      grid.fitted_pieces = [piece, piece, piece, piece]
-      grid.piece_fits?(piece2, 4).should be_true
-    end
-  end
-
-  describe "#solve" do
-    # subject {set_up_grid}
-    it "moves to the next location" do
-      piece_1 = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
-      piece_2 = Piece.new([SPADE, INNER_SPADE, INNER_CLUBS, HEART], 1)
-      grid = Grid.new([piece_1, piece_2])
-      grid.solve
-      grid.fitted_pieces.to_s.should == "[[\"diamond\", \"i_heart\", \"i_diamond\", \"spade\"], [\"spade\", \"i_spade\", \"i_clubs\", \"heart\"]]"
-      grid.location.should == 2
-    end
-
-    it "rotates the piece to make it fit to the next location" do
-      piece_1 = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
-      piece_2 = Piece.new([INNER_SPADE, INNER_CLUBS, HEART, SPADE], 1)
-      grid = Grid.new([piece_1, piece_2])
-      grid.solve
-      grid.fitted_pieces.to_s.should == "[[\"diamond\", \"i_heart\", \"i_diamond\", \"spade\"], [\"spade\", \"i_spade\", \"i_clubs\", \"heart\"]]"
-      grid.location.should == 2
-    end
-
-    it "moves the piece to the last position if it doesn't fit" do
-      piece_1 = Piece.new([INNER_SPADE, INNER_HEART, INNER_DIAMOND, SPADE], 0)
-      piece_2 = Piece.new([INNER_SPADE, INNER_CLUBS, SPADE, SPADE], 1)
-      piece_3 = Piece.new([INNER_SPADE, INNER_CLUBS, HEART, CLUBS], 2)
-      grid = Grid.new([piece_1, piece_2, piece_3])
-      grid.solve
-      grid.fitted_pieces.map(&:id).should == [0,2,1]
-    end
-
-    it "moves the first piece to the last position if it doesn't fit" do
-      piece_1 = Piece.new([DIAMOND, INNER_HEART, INNER_DIAMOND, SPADE], 0)
-      piece_2 = Piece.new([INNER_SPADE, INNER_CLUBS, CLUBS, SPADE], 1)
-      piece_3 = Piece.new([INNER_SPADE, INNER_CLUBS, HEART, CLUBS], 2)
-      grid = Grid.new([piece_2, piece_3, piece_1])
-      grid.solve
-      grid.fitted_pieces.map(&:id).should == [0,2,1]
-    end
+  it "solves" do
+    grid = set_up_grid
+    grid.solve(0)
+    puts grid.fitted_pieces.inspect
+    grid.fitted_pieces.count.should == 9
   end
 end
 
@@ -122,3 +141,15 @@ def set_up_grid
   piece_9 = Piece.new([HEART, INNER_DIAMOND, INNER_CLUBS, CLUBS], 8)
   Grid.new([piece_1, piece_2,piece_3,piece_4,piece_5,piece_6,piece_7,piece_8,piece_9])
 end
+
+[
+  ["spade", "diamond", "i_heart", "i_diamond"], ["i_clubs", "clubs", "heart", "i_diamond"], ["i_clubs", "heart", "diamond", "i_clubs"],
+  ["heart", "spade", "i_spade", "i_clubs"], ["i_heart", "clubs", "heart", "i_spade"], ["i_diamond", "diamond", "clubs", "i_clubs"],
+  ["spade", "diamond", "i_spade", "i_heart"], ["i_heart", "heart", "diamond", "i_diamond"], ["i_clubs", "spade", "spade", "i_heart"]]
+
+
+
+
+
+
+
