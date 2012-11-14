69d0580367c3dd143626650a006049e1c7c63bee
Douglas Roper <douglasroper@notonthehighstreet.com>
2012-11-14 10:11:45 +0000

    refactoring

--- puzzle.rb
+++ puzzle.rb
@@ -27,19 +27,20 @@ class Piece
   end
 end
 
-class Puzzle
-  attr_reader :pieces
+class Grid
+  attr_reader :fitted_pieces
   def initialize(pieces)
     @pieces = pieces
+    @fitted_pieces = []
   end
 
   def solve(loc)
     return true if @pieces.empty?
-    @pieces.each_with_index do |e, i|
+    @pieces.each do
       next unless @fitted_pieces[loc] == nil
       @fitted_pieces[loc] = @pieces.shift
 
-      (0..4).each do |k|
+      (0..4).each do
         return true if piece_fits?(@fitted_pieces[loc], loc) && solve(loc + 1)
         @fitted_pieces[loc].rotate
       end
@@ -54,22 +55,18 @@ class Puzzle
       true
     when 1, 2
       left_right_match?(piece, location)
-    when 3
-      piece.top + @fitted_pieces[0].bottom == 0
-    when 4
-      piece.top + @fitted_pieces[1].bottom == 0 && left_right_match?(piece, location)
-    when 5
-      piece.top + @fitted_pieces[2].bottom == 0 && left_right_match?(piece, location)
-    when 6
-      piece.top + @fitted_pieces[3].bottom == 0
-    when 7
-      piece.top + @fitted_pieces[4].bottom == 0 && left_right_match?(piece, location)
-    when 8
-      piece.top + @fitted_pieces[5].bottom == 0 && left_right_match?(piece, location)
+    when 3, 6
+      top_bottom_match?(piece, location)
+    when 4, 5, 7, 8
+      top_bottom_match?(piece, location) && left_right_match?(piece, location)
     end
   end
 
   def left_right_match?(piece, location)
     piece.left + @fitted_pieces[location - 1].right == 0
+  end
+
+  def top_bottom_match?(piece, location)
+    piece.top + @fitted_pieces[location - 3].bottom == 0
   end
 end
